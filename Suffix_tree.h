175a55e1db5ca220ca948559de4eb9ce978d87cc
Aditya Patil <adityapatil65432@gmail.com>
2022-11-07 02:19:39 +0530

    Added newNode function

--- Suffix_tree.h
+++ Suffix_tree.h
@@ -1,16 +1,17 @@
 #ifndef SUFFIX_TREE_H
 #define SUFFIX_TREE_H
-#define MAX 50
-
-struct SuffixTreeNode{
+#define MAX 256
+int count = 0;
+struct node *root = NULL;
+struct SuffixTreeNode
+{
    struct SuffixTreeNode *suffixLink;
    struct SuffixTreeNode *children[256];
    int suffixindex;
-   int *end ; 
-   int start ; 
-
+   int *end;
+   int start;
 };
 
-int edgeLength(struct SuffixTreeNode*a);
-
+long int edgeLength(struct SuffixTreeNode *a);
+struct SuffixTreeNode *newNode(long int start, long int *end);
 #endif
