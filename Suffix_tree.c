9ee405870c64e2fbaec901f620d185584f90964f
Harsh Raj Srivastava <harshrajsrivastava03@gmail.com>
2022-11-07 04:18:36 +0530

    Added walkDown & setSuffixIndex functions

--- Suffix_tree.c
+++ Suffix_tree.c
@@ -1,24 +1,72 @@
-#include<stdio.h>
-#include"Suffix_tree.h"
-int edgeLength(struct SuffixTreeNode*a)
+#include <stdio.h>
+#include "Suffix_tree.h"
+
+char text[100];
+int count = 0;
+
+int activeLength = 0;
+int activeEdge = -1;
+
+node *root = NULL;
+node *lastNewNode = NULL;
+
+node *activeNode = NULL;
+int activeEdge = -1;
+int activeLength = 0;
+
+int remainingSuffixCount = 0;
+int END = -1;
+int *rootEnd = NULL;
+int size;
+
+node *newNode(int start, int *end)
 {
-   long int e = *(a->end);
-   long int s = a->start;
-   return (e - s + 1);
-}
-struct SuffixTreeNode *newNode(int start, int *end)
-{
-   count = count + 1;
-   struct SuffixTreeNode *STnode = (struct SuffixTreeNode *)malloc(sizeof(struct SuffixTreeNode));
-   long int r = 0;
+   count++;
+   node *STnode = (node *)malloc(sizeof(node));
+   int r = 0;
    while (r < MAX)
-   {
-      STnode->children[r] = NULL;
-      r++;
-   }
+      STnode->children[r++] = NULL;
    STnode->suffixLink = root;
    STnode->start = start;
    STnode->end = end;
    STnode->suffixIndex = -1;
    return STnode;
+}
+
+int edgeLength(node *A)
+{
+   long int e = *(A->end);
+   long int s = A->start;
+   return (e - s + 1);
+}
+
+int walkDown(node *A)
+{
+   // APCFWD - Active point change for walk down using skip/count trick(Trick 1)
+   if (activeLength >= edgeLength(A))
+   {
+      activeNode = A;
+      activeLength -= edgeLength(A);
+      activeEdge = (int)text[activeEdge + edgeLength(A)] - (int)' ';
+      return 1;
+   }
+   return 0;
+}
+
+void print(int i, int j)
+{
+   while (i <= j)
+      printf("%c", text[i++]);
+}
+
+// Setting suffix indices for the suffix tree
+void setSuffixIndex(node *n, int labelHeight)
+{
+   if (n == NULL) 
+      return;
+   for (int i = 0; i < MAX; i++)
+   {
+      if (n->children[i] != NULL)
+         setSuffixIndex(n->children[i], labelHeight + edgeLength(n->children[i]));
+   }
 }
--- Suffix_tree.h
+++ Suffix_tree.h
@@ -1,19 +1,17 @@
 #ifndef SUFFIX_TREE_H
 #define SUFFIX_TREE_H
 #define MAX 256
-int count = 0;
-struct node *root = NULL;
-struct SuffixTreeNode
+typedef struct SuffixTreeNode
 {
    struct SuffixTreeNode *suffixLink;
-   struct SuffixTreeNode *children[256];
+   struct SuffixTreeNode *children[MAX];
    int suffixIndex;
    int *end ; 
    int start ; 
-};
-int activeLength;
-int activeEdge;
+} node;
 
-int edgeLength(struct SuffixTreeNode *a);
 struct SuffixTreeNode *newNode(int start,int *end);
+int edgeLength(struct SuffixTreeNode *a);
 #endif
+
+
