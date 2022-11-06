7e994e9dad59aec468f19faf602c3cfa6640c464
alankritkadian <alankritkadian@gmail.com>
2022-11-07 02:26:37 +0530

    merge

--- Suffix_tree.h
+++ Suffix_tree.h
@@ -1,15 +1,16 @@
 #ifndef SUFFIX_TREE_H
 #define SUFFIX_TREE_H
-#define MAX 50
+#define MAX 256
 
 struct SuffixTreeNode{
    struct SuffixTreeNode *suffixLink;
    struct SuffixTreeNode *children[256];
-   int suffixindex;
+   int suffixIndex;
    int *end ; 
    int start ; 
-
 };
+int activeLength;
+int activeEdge;
 
 int edgeLength(struct SuffixTreeNode*a);
 
