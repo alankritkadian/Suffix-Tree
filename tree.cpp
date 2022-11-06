2fbe5c372af9f5338e841c7d2f6477d53ffb7705
Aditya Patil <adityapatil65432@gmail.com>
2022-11-06 16:39:09 +0530

    Added SuffixTreeNode and A function to calculate edgelength

--- tree.cpp
+++ tree.cpp
@@ -1,4 +1,23 @@
 #include<bits/stdc++.h>
+
+#define MAX 256
+struct SuffixTreeNode{
+   struct SuffixTreeNode *suffixLink;
+   struct SuffixTreeNode *children[MAX];
+   int suffixindex;
+   int *end ; 
+   int start ; 
+
+};
+
+
+
+int edgeLength(struct SuffixTreeNode*a)
+{
+  int e=*(n->end);
+  int s= n->start ; 
+  return (e-s+1);
+}
 int main(void){
   return 0;
 }
